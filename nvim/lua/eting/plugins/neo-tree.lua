776bcaa6e7b2616eb67f4719a00b07ac79f7f3e8
evelynting610 <esting610@gmail.com>
2024-01-20 21:50:10 -0600

    Update neotree

--- nvim/lua/eting/plugins/neo-tree.lua
+++ nvim/lua/eting/plugins/neo-tree.lua
@@ -8,6 +8,7 @@ return {
 		-- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
 	},
 	config = function()
+		vim.keymap.set("n", "<leader>tr", "<cmd>Neotree toggle<cr>")
 		require("neo-tree").setup({
 			close_if_last_window = true,
 			enable_git_status = false,
@@ -21,6 +22,17 @@ return {
 				},
 			},
 			use_libuv_file_watcher = true,
+			window = {
+				auto_expand_width = true,
+			},
+			event_handlers = {
+				{
+					event = "file_opened",
+					handler = function(_file_path)
+						require("neo-tree.command").execute({ action = "close" })
+					end,
+				},
+			},
 		})
 	end,
 }
